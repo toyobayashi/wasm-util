4ee985bfbef1ca07f87903e303931275e2a994a3
toyobayashi <lifenglin314@outlook.com>
2022-10-28 22:01:41 +0800

    test default stdin message

--- test/stdin/index.js
+++ test/stdin/index.js
@@ -3,6 +3,10 @@
 describe('stdin', function () {
   it('stdin', async function () {
     this.timeout = Infinity
+    const prompt = window.prompt
+    window.prompt = function (message, defaultValue) {
+      return prompt.call(window, 'Test stdin', 'Hello, stdin!')
+    }
     const wasi = new wasmUtil.WASI({
       returnOnExit: true
     })
@@ -11,5 +15,6 @@ describe('stdin', function () {
     })
 
     assert(0 === wasi.start(instance))
+    window.prompt = prompt
   })
 })
