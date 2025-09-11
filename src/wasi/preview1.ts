70f23296562c19ba3a00b9294532c1ebf7090b05
Toyo Li <lifenglin314@outlook.com>
2025-09-11 22:25:52 +0800

    fix: do not throw if `process` is undefined (#5)

--- src/wasi/preview1.ts
+++ src/wasi/preview1.ts
@@ -116,26 +116,45 @@ function defineName<T extends Function> (name: string, f: T): T {
   return f
 }
 
+function tryCall<T extends (this: WASI, ...args: any[]) => WasiErrno | PromiseLike<WasiErrno>> (f: T, wasi: WASI, args: any[]): WasiErrno | PromiseLike<WasiErrno> {
+  let r: WasiErrno | PromiseLike<WasiErrno>
+  try {
+    r = f.apply(wasi, args)
+  } catch (err: any) {
+    return handleError(err)
+  }
+
+  if (isPromiseLike(r)) {
+    return r.then(_ => _, handleError)
+  }
+  return r
+}
+
 function syscallWrap<T extends (this: WASI, ...args: any[]) => WasiErrno | PromiseLike<WasiErrno>> (self: WASI, name: string, f: T): T {
-  return defineName(name, function () {
-    if (process.env.NODE_DEBUG_NATIVE === 'wasi') {
+  let debug = false
+
+  const NODE_DEBUG_NATIVE = (() => {
+    try {
+      return process.env.NODE_DEBUG_NATIVE
+    } catch (_) {
+      return undefined
+    }
+  })()
+  if (typeof NODE_DEBUG_NATIVE === 'string' && NODE_DEBUG_NATIVE.split(',').includes('wasi')) {
+    debug = true
+  }
+
+  return debug
+    ? defineName(name, function () {
       const args = Array.prototype.slice.call(arguments)
       let debugArgs = [`${name}(${Array.from({ length: arguments.length }).map(() => '%d').join(', ')})`]
       debugArgs = debugArgs.concat(args)
       console.debug.apply(console, debugArgs)
-    }
-    let r: WasiErrno | PromiseLike<WasiErrno>
-    try {
-      r = f.apply(self, arguments as any)
-    } catch (err: any) {
-      return handleError(err)
-    }
-
-    if (isPromiseLike(r)) {
-      return r.then(_ => _, handleError)
-    }
-    return r
-  }) as unknown as T
+      return tryCall(f, self, args)
+    }) as unknown as T
+    : defineName(name, function () {
+      return tryCall(f, self, arguments as any)
+    }) as unknown as T
 }
 
 function resolvePathSync (fs: IFs, fileDescriptor: FileDescriptor, path: string, flags: number): string {
